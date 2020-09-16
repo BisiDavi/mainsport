02f736114c056e0af821d369875b95f2db20998e
BisiDavi <oludavidconnect@gmail.com>
2020-09-16 06:20:52 +0100

    .'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+Route::get('/', function(){
+    return view('welcome');
+});
 
 Route::get('/', 'HomeController@index');
 
